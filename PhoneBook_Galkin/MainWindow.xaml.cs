5c8da2366cb9a153c9c03d69cef2774139a5caf2
student-a310 <student-a310@PC-A310-9>
2023-12-07 10:18:09 +0500

    Разработанное окно MainWindow. Galkin

--- PhoneBook_Galkin/MainWindow.xaml.cs
+++ PhoneBook_Galkin/MainWindow.xaml.cs
@@ -1,28 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace PhoneBook_Galkin
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+     /// <summary>
+     /// Логика взаимодействия для MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window
 	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+          public static Connection connect;
+          public static Pages.Main.main;
+        public MainWindow()
+          {
+               InitializeComponent();
+               connect = new Connection();
+               connect.LoadData(Connections.tabels.users);
+               connect.LoadData(Connection.tabels.calls);
+               main = new Pages.Main();
+               OpenPageMain();
+          }
+
+          public void OpenPageMain()
+          {
+               DoubleAnimation opgridAnimation = new DoubleAnimation();
+               opgridAnimation.From = 1;
+               opgridAnimation.To = 0;
+               opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+               opgridAnimation.Completed += delegate
+               {
+                    frame.Navigate(main);
+                    opgridAnimation.From = 0;
+                    opgridAnimation.To = 1;
+                    opgridAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                    frame.BeginAnimation(Frame.OpacityMaskProperty, opgridAnimation);
+               };
+               frame.BeginAnimation(Frame.OpacityMaskProperty, opgridAnimation);
+          }
+     }
 }
